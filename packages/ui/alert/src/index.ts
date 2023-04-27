7095a9783fe4acd3fbc0c38bda7f1f574cb569ce
zhouyun1 <zhouyun1@xiaomi.com>
2023-04-27 14:37:04 +0800

    chore: test

--- packages/ui/alert/src/index.ts
+++ packages/ui/alert/src/index.ts
@@ -5,10 +5,12 @@ export { Alert as default } from './Alert'
 
 export * from './types'
 console.log('', a)
-
+// 什么
 function c() {
   const b = []
+
   if (b.length === 0) {
     console.log(c)
   }
+  if () {}
 }
